bd4f1c31c5a4c8869d9ef8ed21ba37c8bfe95594
sergo666 <serrrfff@gmail.com>
2017-05-04 21:32:16 +0700

    Update_demo

--- bc.c
+++ bc.c
@@ -1,73 +1,74 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <locale.h> 
-#include <conio.h> 
+//#include <conio.h> 
 #include <time.h> 
 
 void main()
 {
-srand(time(NULL));
-int a, b, c, d;
+    srand(time(NULL));
+    int a, b, c, d;	//рандомим число по разрядам
 
 
-a = 0 + rand() % 9;
+    a = 0 + rand() % 9;
 
-do
-{
-b = 0 + rand() % 9;
-} while (b == a);
+    do
+    {
+        b = 0 + rand() % 9;
+    } while (b == a);
 
-do
-{
-c = 0 + rand() % 9;
-} while (c == a || c == b);
+    do
+    {
+        c = 0 + rand() % 9;
+    } while (c == a || c == b);
 
-do
-{
-d = 0 + rand() % 9;
-} while (d == a || d == b || d == c);
-
-
-int ch = 0, u, x, y, z;
-int bu = 0, co = 0;
-printf("Rand ch %d\n", a * 1000 + b * 100 + c * 10 + d);
-while (ch != (a * 1000 + b * 100 + c * 10 + d))
-{
-printf("Vvedite chetyrehznachnoe chislo = ");
-scanf("%d", &ch); 
-printf("\n");
-u = ch / 1000;
-x = ch / 100 % 10;
-y = ch / 10 % 10;
-z = ch % 10;
-
-if (u == a)
-bu++;
-if (x == b)
-bu++;
-if (y == c)
-bu++;
-if (z == d)
-bu++;
-
-if (u == b || u == c || u == d)
-co++;
-if (x == a || x == c || x == d)
-co++;
-if (y == b || y == a || y == d)
-co++;
-if (z == b || z == c || z == a)
-co++;
-
-printf("%d bull\n", bu);
-printf("%d corova\n", co);
-printf("\n");
-printf("\n");
-co = 0;
-bu = 0;
-}
+    do
+    {
+        d = 0 + rand() % 9;
+    } while (d == a || d == b || d == c);
 
 
+    int number = 0, fst, scnd, thrd, frth;
+    int bulls = 0, cows = 0;
+    printf("Rand number %d\n", a * 1000 + b * 100 + c * 10 + d);
+    
+    while (number != (a * 1000 + b * 100 + c * 10 + d))
+    {
+        printf("Vvedite chetyrehznachnoe chislo = ");
+        scanf("%d", &number); 
+        printf("\n");
+        fst = number / 1000;
+        scnd = number / 100 % 10;
+        thrd = number / 10 % 10;
+        frth = number % 10;
 
-_getch();
+        if (fst == a)
+            bulls++;
+        if (scnd == b)
+            bulls++;
+        if (thrd == c)
+            bulls++;
+        if (frth == d)
+            bulls++;
+
+        if (fst == b || fst == c || fst == d)
+            cows++;
+        if (scnd == a || scnd == c || scnd == d)
+            cows++;
+        if (thrd == b || thrd == a || thrd == d)
+            cows++;
+        if (frth == b || frth == c || frth == a)
+            cows++;
+
+        printf("%d bull's\n", bulls);
+        printf("%d cow's\n", cows);
+        printf("\n");
+        printf("\n");
+        cows = 0;
+        bulls = 0;
+    }
+
+    printf("Congratulations\n");
+
+    //_getch();
 }

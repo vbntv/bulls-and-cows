c80adf8928f8422b7399518f66066a352833679a
vbntv <vbuntovv@mail.ru>
2017-05-29 19:27:56 +0700

    Fix bugs

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <locale.h> 
-//#include <conio.h> 
 #include <time.h> 
 #include "function.h"
 
 int main()
 {
-menu();
-
-return 0;   
+    menu();
+    return 0;
 }
